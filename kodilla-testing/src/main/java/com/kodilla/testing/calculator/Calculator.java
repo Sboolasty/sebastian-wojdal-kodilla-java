6d81f4dc1ca14ea4525c7d973857eb3673a0c712
Sebastian Wojdal <sebastianwojdal@wp.pl>
2023-01-18 18:12:49 +0100

    6.2 First Test

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,18 @@
+package com.kodilla.testing.calculator;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+public class Calculator {
+            public int addAToB(int a, int b) {
+            return a + b;
+        }
+
+        public int subtractAFromB(int a, int b) {
+            return b - a;
+        }
+
+    }
+
