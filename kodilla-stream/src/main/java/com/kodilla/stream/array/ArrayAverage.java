2ba27674809faea878c6dbcc841dcf1b3f0f0cec
Sboolasty <sebastianwojdal@wp.pl>
2023-01-31 19:16:16 +0100

    7.5 V2

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayAverage.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayAverage.java
@@ -5,7 +5,9 @@ import java.util.stream.IntStream;
 public class ArrayAverage implements ArrayOperations {
 
     public double getAverage(int[] numbers) {
-        IntStream.of(numbers).forEach(System.out::println);
+        IntStream.range(0,numbers.length)
+                .map(n -> numbers[n])
+                .forEach(System.out::println);
 
         double average = IntStream.range(0, numbers.length)
                 .mapToDouble(k -> numbers[k])
