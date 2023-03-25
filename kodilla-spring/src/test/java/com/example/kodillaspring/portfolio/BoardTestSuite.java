3f12570ff3e310b29b5325a3c99dd315fa5c7776
Sboolasty <sebastianwojdal@wp.pl>
2023-03-25 13:10:57 +0100

    14.3 v1

---
+++ kodilla-spring/src/test/java/com/example/kodillaspring/portfolio/BoardTestSuite.java
@@ -0,0 +1,36 @@
+package com.example.kodillaspring.portfolio;
+import com.example.portfolio.Board;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class BoardTestSuite {
+   @Autowired
+    private Board board;
+
+    @Test
+    public void testTaskAdd() {
+        // given
+        String taskToDo = "Task to do";
+        String taskInProgress = "Task in progress";
+        String taskDone = "Task done";
+
+        // when
+        board.getToDoList().getTasks().add(taskToDo);
+        board.getInProgressList().getTasks().add(taskInProgress);
+        board.getDoneList().getTasks().add(taskDone);
+
+        // then
+        assertEquals(1, board.getToDoList().getTasks().size());
+        assertEquals(taskToDo, board.getToDoList().getTasks().get(0));
+
+        assertEquals(1, board.getInProgressList().getTasks().size());
+        assertEquals(taskInProgress, board.getInProgressList().getTasks().get(0));
+
+        assertEquals(1, board.getDoneList().getTasks().size());
+        assertEquals(taskDone, board.getDoneList().getTasks().get(0));
+    }
+}
