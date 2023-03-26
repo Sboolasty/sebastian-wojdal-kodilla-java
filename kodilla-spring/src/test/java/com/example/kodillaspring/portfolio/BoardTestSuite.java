7ea3676041da5044806a4be9171563270b8f4a5d
Sboolasty <sebastianwojdal@wp.pl>
2023-03-26 16:05:38 +0200

    14.4

--- kodilla-spring/src/test/java/com/example/kodillaspring/portfolio/BoardTestSuite.java
+++ kodilla-spring/src/test/java/com/example/kodillaspring/portfolio/BoardTestSuite.java
@@ -1,14 +1,12 @@
 package com.example.kodillaspring.portfolio;
 import com.example.portfolio.Board;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class BoardTestSuite {
-   @Autowired
     private Board board;
 
     @Test
